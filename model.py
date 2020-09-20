6545a8165e622f5dc27ce34f8b3cece9b7eb832f
Flock-Anizak <sarvagya.gupta18@gmail.com>
2020-09-20 18:32:23 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,8 +3,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torchaudio
-
 from torch.autograd import Variable
 from torch.utils.data import DataLoader, Dataset
 
